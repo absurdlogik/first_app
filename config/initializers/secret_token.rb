3fe4b12d527157547a31c63a1963934a054f73dc
Absurd Logik <absurd.logik@gmail.com>
2015-05-17 22:28:50 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '9906e54d154ca33f9f61e5b8413223c093e27438ef0a05746f17ea716e0731e9d16b3d5e43b7013fed09eb61a7be3486a810f67413d4e0b8c81e6809949ef022'
